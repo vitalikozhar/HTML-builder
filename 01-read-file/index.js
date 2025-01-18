8d2e7e77c3e87f57e614bb5b5766044a45464ff6
Vitali Kozhar <voxfree@pm.me>
2025-01-18 17:37:20 +0400

    feat: solution of the first task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const fs = require("fs");
+
+const filePath = path.join(__dirname, "text.txt");
+const fileReadStream = fs.createReadStream(filePath, {encoding: "utf-8"});
+let text = "";
+
+fileReadStream.on("data", (chunk) => {
+    text += chunk;
+});
+
+fileReadStream.on("end", () => {
+    console.log(text);
+});
