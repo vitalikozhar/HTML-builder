047c13e278d49a94141cddca98e5231926610ab9
Vitali Kozhar <voxfree@pm.me>
2025-01-21 18:31:19 +0400

    fix: fix bag in task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,35 +1,33 @@
 const path = require('path');
 const fs = require('fs');
+
 const oldFold = path.join(__dirname, 'files');
 const newFold = path.join(__dirname, 'files-copy');
 
-if (fs.existsSync(newFold)) {
-  fs.rm(newFold, { recursive: true }, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+fs.access(newFold, fs.constants.F_OK, (err) => {
+  if (!err) {
+    fs.rm(newFold, { recursive: true }, () => {
+      copyFolder();
+    });
+  } else {
     copyFolder();
-  });
-} else {
-  copyFolder();
-}
+  }
+});
 
 function copyFolder() {
   fs.mkdir(newFold, { recursive: true }, () => {
     fs.readdir(oldFold, { withFileTypes: true }, (err, items) => {
       if (err) {
         console.log('Error: Folder not found or cannot be read:', err);
-      } else {
-        for (const item of items) {
-          const oldLink = path.join(oldFold, item.name);
-          const newLink = path.join(newFold, item.name);
-
-          if (item.isFile()) {
-            fs.copyFile(oldLink, newLink, () => {
-              console.log(`File "${item.name}" has been successfully copied.`);
-            });
-          }
+        return;
+      }
+      for (const item of items) {
+        const oldLink = path.join(oldFold, item.name);
+        const newLink = path.join(newFold, item.name);
+        if (item.isFile()) {
+          fs.copyFile(oldLink, newLink, () => {
+            console.log(`File "${item.name}" has been successfully copied.`);
+          });
         }
       }
     });
